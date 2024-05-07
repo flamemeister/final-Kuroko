e26b6d219840e5bc3266a5d52bfe6ab8db8ccb5b
samxael <aldiyar.saken123@gmail.com>
2024-05-07 22:57:07 +0500

    feat: login function

--- server/src/app.js
+++ server/src/app.js
@@ -5,20 +5,16 @@ const admin = require('firebase-admin');
 const app = express();
 const port = 8080;
 
-// Middleware
 app.use(bodyParser.json());
 
-// Initialize Firebase Admin SDK
 const serviceAccount = require('./kurokoserver-3156e-firebase-adminsdk-2btbq-8d275f6b6b');
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
 
-// Routes
 const authRoutes = require('./routes/authRoute');
 app.use('/auth', authRoutes);
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -1,17 +1,15 @@
+// authController.js
 const admin = require('firebase-admin');
 const db = admin.firestore();
 
 exports.registerUser = async (req, res) => {
   try {
     const { email, password } = req.body;
-    
-    // Create user with Firebase Authentication
     const userRecord = await admin.auth().createUser({
       email,
       password
     });
 
-    // Optionally, save user information to Firestore
     await db.collection('users').doc(userRecord.uid).set({
       email
     });
@@ -22,3 +20,21 @@ exports.registerUser = async (req, res) => {
     res.status(500).send(error);
   }
 };
+
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const userRecord = await admin.auth().getUserByEmail(email);
+
+    await admin.auth().updateUser(userRecord.uid, { password });
+
+    res.status(200).send(`User ${userRecord.uid} logged in successfully`);
+  } catch (error) {
+    console.error('Error logging in:', error);
+    res.status(401).send('Invalid email or password');
+  }
+};
+
+
+
